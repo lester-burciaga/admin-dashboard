fa4bf7ffd28c965e7edc76071f7d87c8d1d548b0
lesterbv06 <bloodshot_x@hotmail.com>
2024-07-29 09:52:42 -0600

    feat: implementation of auth page & aut layout

---
+++ app/(auth)/auth/page.tsx
@@ -0,0 +1,12 @@
+/**
+ *
+ * Auth Page
+ *
+ * @description This page needs to be in a folder named `auth`
+ * to prevent namespace collisions with 'main' page.
+ */
+function AuthPage() {
+  return <div>Auth</div>;
+}
+
+export default AuthPage;
