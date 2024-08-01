4608c932207953264a497c03a9d2aa18cdce7c3d
lesterbv06 <bloodshot_x@hotmail.com>
2024-08-01 11:03:57 -0600

    fix: theme toggler, navbar, postForm & registerForm test fixes

--- components/organisms/Navbar/Navbar.test.tsx
+++ components/organisms/Navbar/Navbar.test.tsx
@@ -1,27 +1,39 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Navbar from './Navbar';
 import { describe } from 'node:test';
 
 jest.mock('../ui/dropdown-menu', () => {
-  const originalModule = jest.requireActual('../ui/dropdown-menu');
+  const originalModule = jest.requireActual(
+    '../ui/dropdown-menu'
+  );
   return {
     ...originalModule,
-    DropdownMenu: ({ children }: { children: React.ReactNode }) => (
-      <div>{children}</div>
-    ),
-    DropdownMenuTrigger: ({ children }: { children: React.ReactNode }) => (
-      <div>{children}</div>
-    ),
-    DropdownMenuContent: ({ children }: { children: React.ReactNode }) => (
-      <div>{children}</div>
-    ),
-    DropdownMenuItem: ({ children }: { children: React.ReactNode }) => (
-      <div>{children}</div>
-    ),
-    DropdownMenuLabel: ({ children }: { children: React.ReactNode }) => (
-      <div>{children}</div>
-    ),
+    DropdownMenu: ({
+      children,
+    }: {
+      children: React.ReactNode;
+    }) => <div>{children}</div>,
+    DropdownMenuTrigger: ({
+      children,
+    }: {
+      children: React.ReactNode;
+    }) => <div>{children}</div>,
+    DropdownMenuContent: ({
+      children,
+    }: {
+      children: React.ReactNode;
+    }) => <div>{children}</div>,
+    DropdownMenuItem: ({
+      children,
+    }: {
+      children: React.ReactNode;
+    }) => <div>{children}</div>,
+    DropdownMenuLabel: ({
+      children,
+    }: {
+      children: React.ReactNode;
+    }) => <div>{children}</div>,
     DropdownMenuSeparator: () => <hr />,
   };
 });
@@ -42,15 +54,15 @@ describe('<Navbar />', () => {
     });
 
     it('should checks dropdown menu links', async () => {
-      expect(screen.getByText(/my account/i)).toBeInTheDocument();
-      expect(screen.getByRole('link', { name: /profile/i })).toHaveAttribute(
-        'href',
-        '/profile'
-      );
-      expect(screen.getByRole('link', { name: /logout/i })).toHaveAttribute(
-        'href',
-        '/auth'
-      );
+      expect(
+        screen.getByText(/my account/i)
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole('link', { name: /profile/i })
+      ).toHaveAttribute('href', '/profile');
+      expect(
+        screen.getByRole('link', { name: /logout/i })
+      ).toHaveAttribute('href', '/auth');
     });
   });
 });
