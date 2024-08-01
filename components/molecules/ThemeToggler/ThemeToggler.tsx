eddd9136eb8d66fb26f3f32007934d486c2ade13
lesterbv06 <bloodshot_x@hotmail.com>
2024-08-01 09:46:17 -0600

    feat: implementation of theme toggler on auth page

--- components/molecules/ThemeToggler/ThemeToggler.tsx
+++ components/molecules/ThemeToggler/ThemeToggler.tsx
@@ -25,7 +25,7 @@ export default function ThemeToggler() {
       <DropdownMenuTrigger asChild>
         <Button
           variant='outline'
-          size={null}
+          size={'icon'}
           className='bg-slate-700 hover:bg-slate-600 border-0 mr-5'
         >
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
