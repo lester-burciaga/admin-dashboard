c35335050eee3d33a1f7889250c7ca08122093de
lesterbv06 <bloodshot_x@hotmail.com>
2024-08-02 10:10:38 -0600

    feat: deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  basePath: '/admin-dashboard',
+  output: 'export',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
